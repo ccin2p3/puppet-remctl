01f176e0b6570f7aa0df939df8c1daa46b28a068
Rémi Ferrand <remi.ferrand@cc.in2p3.fr>
2024-04-04 09:32:16 +0200

    feat: After pdk convert

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,58 +1,74 @@
-dir = File.expand_path(File.dirname(__FILE__))
-$LOAD_PATH.unshift File.join(dir, 'lib')
+# frozen_string_literal: true
 
-# Use latest version of rspec-puppet
-$LOAD_PATH.unshift File.join(dir, 'fixtures/modules/rspec-puppet/lib')
-
-require 'rubygems'
-require 'rspec-puppet'
-require 'puppetlabs_spec_helper/module_spec_helper'
-
-begin
-    require 'simplecov'
-
-    SimpleCov.start do
-        add_filter 'fixtures/modules/'
-    end
-rescue Exception => e
-    warn 'Code coverage has been disabled'
+RSpec.configure do |c|
+  c.mock_with :rspec
 end
 
-# RSpec.configure do |c|
-#     c.filter_run :focus => true
-# end
+require 'puppetlabs_spec_helper/module_spec_helper'
+require 'rspec-puppet-facts'
 
-@common_specs = {
-    :concat_basedir             => '/tmp',
+require 'spec_helper_local' if File.file?(File.join(File.dirname(__FILE__), 'spec_helper_local.rb'))
 
-    :basedir                    => '/etc/remctl',
-    :confdir                    => '/etc/remctl/conf.d',
-    :conffile                   => '/etc/remctl/remctl.conf',
-    :acldir                     => '/etc/remctl/acl',
+include RspecPuppetFacts
 
-    :krb5_keytab                => '/etc/krb5.keytab',
-    :server_bin                 => '/usr/sbin/remctld',
-
-    :debug                      => false,
-    :port                       => 4373,
-    :disable                    => true
+default_facts = {
+  puppetversion: Puppet.version,
+  facterversion: Facter.version,
 }
 
-@oses_specs = {
+default_fact_files = [
+  File.expand_path(File.join(File.dirname(__FILE__), 'default_facts.yml')),
+  File.expand_path(File.join(File.dirname(__FILE__), 'default_module_facts.yml')),
+]
 
-    'RedHat'            => @common_specs.merge({
-        :operatingsystem            => 'RedHat',
-        :osfamily                   => 'RedHat',
+default_fact_files.each do |f|
+  next unless File.exist?(f) && File.readable?(f) && File.size?(f)
 
-        :remctl_client_package      => 'remctl',
-        :remctl_server_package      => 'remctl',
-    }),
+  begin
+    default_facts.merge!(YAML.safe_load(File.read(f), permitted_classes: [], permitted_symbols: [], aliases: true))
+  rescue StandardError => e
+    RSpec.configuration.reporter.message "WARNING: Unable to load #{f}: #{e}"
+  end
+end
 
-    'Debian'            => @common_specs.merge({
-        :operatingsystem            => 'Debian',
-        :osfamily                   => 'Debian',
+# read default_facts and merge them over what is provided by facterdb
+default_facts.each do |fact, value|
+  add_custom_fact fact, value
+end
 
-        :remctl_client_package      => 'remctl-client',
-        :remctl_server_package      => 'remctl-server',
-    }),
-}
+RSpec.configure do |c|
+  c.default_facts = default_facts
+  c.before :each do
+    # set to strictest setting for testing
+    # by default Puppet runs at warning level
+    Puppet.settings[:strict] = :warning
+    Puppet.settings[:strict_variables] = true
+  end
+  c.filter_run_excluding(bolt: true) unless ENV['GEM_BOLT']
+  c.after(:suite) do
+    RSpec::Puppet::Coverage.report!(0)
+  end
+
+  # Filter backtrace noise
+  backtrace_exclusion_patterns = [
+    %r{spec_helper},
+    %r{gems},
+  ]
+
+  if c.respond_to?(:backtrace_exclusion_patterns)
+    c.backtrace_exclusion_patterns = backtrace_exclusion_patterns
+  elsif c.respond_to?(:backtrace_clean_patterns)
+    c.backtrace_clean_patterns = backtrace_exclusion_patterns
+  end
+end
+
+# Ensures that a module is defined
+# @param module_name Name of the module
+def ensure_module_defined(module_name)
+  module_name.split('::').reduce(Object) do |last_module, next_module|
+    last_module.const_set(next_module, Module.new) unless last_module.const_defined?(next_module, false)
+    last_module.const_get(next_module, false)
+  end
+end
+
+# 'spec_overrides' from sync.yml will appear below this line
